17bc955c5e81dfd56d62e3711690a57b19210428
darkchoco <darkchoco.ice@gmail.com>
2022-03-11 13:42:34 +0100

    main: Implement Taco Web Application (Basic) - closes #1

---
+++ taco/src/main/java/darkchoco/taco/web/DesignTacoController.java
@@ -0,0 +1,82 @@
+package darkchoco.taco.web;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import darkchoco.taco.Ingredient;
+import darkchoco.taco.Ingredient.Type;
+import darkchoco.taco.Order;
+import darkchoco.taco.Taco;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.validation.Errors;
+
+import javax.validation.Valid;
+
+@Slf4j  // by Lombok
+@Controller
+@RequestMapping("/design")  // 'design'으로 시작되는 경로의 요청 처리.
+@SessionAttributes("order")
+public class DesignTacoController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+    }
+
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute Order order) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        order.addTaco(taco);
+
+        log.info("Processing design: {}", taco);
+
+        return "redirect:/orders/current";
+    }
+}
---
+++ taco/src/main/java/darkchoco/taco/web/OrderController.java
@@ -0,0 +1,41 @@
+package darkchoco.taco.web;
+
+import darkchoco.taco.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+}
